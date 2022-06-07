eabf79a85b01616078c41703e0f0189cd4a32cca
Samuel LANGUET <contact@languet.fr>
2022-06-07 14:42:54 +0200

    nettoyage de code

--- src/App.js
+++ src/App.js
@@ -22,4 +22,15 @@ function App() {
   );
 }
 
+function Footer(){
+  return (
+    <div className="Foot">
+    <footer className="Foot-footer">
+      <p>le nom de votre compagnie</p>
+      <img src={logo} className="Foot-logo" alt="logo" />
+
+    </footer>  
+    </div>
+    );
+}
 export default App;
