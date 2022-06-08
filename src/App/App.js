2aa18af60de83cd0cdd400130a2437230a1889fb
Samuel LANGUET <contact@languet.fr>
2022-06-08 15:25:32 +0200

    Ajout des pros dans le composant Message

--- src/App/App.js
+++ src/App/App.js
@@ -3,13 +3,14 @@ import Emoji from '../Emoji/Emoji';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import Produit from '../Produit/Produit';
+import Message from '../Message/Message';
 import './App.css';
 
 export default function App() {
 
   // const entete = 'titre';
   // const titre = <h1>Un titre h1</h1>;
-
+ 
   return (
       <div className="App">
         {/* Ceci est un commentaire en JSX */}
@@ -18,9 +19,13 @@ export default function App() {
       {/* <h1 className={entete}>Titre React</h1>
       {titre} */}
       <Produit />
+      <Message messagePasse="la" prenom='Pierre'/>
+      <Message messagePasse="planete" prenom='Paul'/>
+      <Message messagePasse="est bleu" prenom='Matt'/>
       <Contenu />
       <Footer />
       </div>
   );
 }
 
+ 
