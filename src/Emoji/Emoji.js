c7d0a034246e3262d350526a339cef3674b4db21
Samuel LANGUET <contact@languet.fr>
2022-06-08 11:40:44 +0200

    ajout boutton

--- src/Emoji/Emoji.js
+++ src/Emoji/Emoji.js
@@ -1,23 +1,72 @@
 import './Emoji.css';
 
+// const montrerEmoji =event => alert(event.target.id);
+
+// export default function Emoji() {
+//     return (
+//         <ul>
+//             <li>
+//                 <button onClick={event => alert(event.target.id)}>
+//                 <span role="omg" id="sourire">😀</span>
+//                 </button>
+//             </li>
+//             <li>
+//                 <button onClick={montrerEmoji}>
+//                 <span role="omg" id="animal">🦁</span>
+//                 </button>
+//             </li>
+//             <li>
+//                 <button onClick={event => alert(event.target.id)}>
+//                 <span role="omg" id="nouriture">🍉</span>
+//                 </button>
+//             </li>
+//         </ul>
+//     )
+// };
+
+const emojis = [
+    {
+    emoji: "😀",
+    name: "sourire"
+    },
+    {
+    emoji: "🦁",
+    name: "animal"    
+    },
+    {
+        emoji: "🍉",
+        name: "nouriture"    
+        },
+];
+
+const montrerEmoji =event => alert(event.target.id);
 export default function Emoji() {
-    return (
+    return(
+        <>
         <ul>
-            <li>
-                <button onClick={event => alert(event.target.id)}>
-                <span id="sourire">😀</span>
-                </button>
-            </li>
-            <li>
-                <button onClick={event => alert(event.target.id)}>
-                <span id="animal">🦁</span>
-                </button>
-            </li>
-            <li>
-                <button onClick={event => alert(event.target.id)}>
-                <span id="nouriture">🍉</span>
-                </button>
-            </li>
+            {
+                emojis.map(emoji => (
+                        <li key={emoji.emoji}>
+                            <button onClick={montrerEmoji}>
+                            <span role="img" id={emoji.name}>🦁</span>
+                            </button>
+                        </li>
+                ))
+            }
         </ul>
+        </>
     )
-};
+}
+// const planetes = [
+//     "Terre",
+//     "Saturne",
+//     "Pluton"
+// ];
+
+// export default function Emoji() {
+//     return(
+//         <>
+//         {planetes.map(planete => <div key={planete}>{planete}</div>)}
+//         </>
+//     )
+// }
